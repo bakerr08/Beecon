abd9faedee046392ba9d417c24822c4f5a54d671
bakerr08 <bakerr08@gmail.com>
2014-02-06 10:10:49 -0600

    JSON Demo

---
+++ JSONDemo/JSONDemo/JSONTest.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace JSONDemo
+{
+    partial class JSONTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTEST = new System.Windows.Forms.Button();
+            this.btnOFF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTEST
+            // 
+            this.btnTEST.Location = new System.Drawing.Point(90, 61);
+            this.btnTEST.Name = "btnTEST";
+            this.btnTEST.Size = new System.Drawing.Size(99, 63);
+            this.btnTEST.TabIndex = 0;
+            this.btnTEST.Text = "TEST";
+            this.btnTEST.UseVisualStyleBackColor = true;
+            this.btnTEST.Click += new System.EventHandler(this.btnTEST_Click);
+            // 
+            // btnOFF
+            // 
+            this.btnOFF.Location = new System.Drawing.Point(90, 149);
+            this.btnOFF.Name = "btnOFF";
+            this.btnOFF.Size = new System.Drawing.Size(99, 63);
+            this.btnOFF.TabIndex = 1;
+            this.btnOFF.Text = "Off";
+            this.btnOFF.UseVisualStyleBackColor = true;
+            this.btnOFF.Click += new System.EventHandler(this.btnOFF_Click);
+            // 
+            // JSONTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnOFF);
+            this.Controls.Add(this.btnTEST);
+            this.Name = "JSONTest";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTEST;
+        private System.Windows.Forms.Button btnOFF;
+    }
+}
+
---
+++ JSONDemo/JSONDemo/JSONTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace JSONDemo
+{
+    public partial class JSONTest : Form
+    {
+        public JSONTest()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTEST_Click(object sender, EventArgs e)
+        {
+            string host = "10.4.134.123:8124";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://" +host + "?cmd=control");
+            request.Method = "POST";
+            request.ContentType = "text/html";
+
+            string postJSON = "{\"mode\":\"control\",\"lights\":[{\"id\":\"6\",\"state\":\"on\"}]}";
+
+            byte[] buffer = Encoding.ASCII.GetBytes(postJSON);
+            Stream postData = request.GetRequestStream();
+            postData.Write(buffer, 0, buffer.Length);
+            postData.Close();
+
+            Stream response = request.GetResponse().GetResponseStream();
+            StreamReader responseReader = new StreamReader(response);
+            string responseJSON = responseReader.ReadToEnd();
+        }
+
+        private void btnOFF_Click(object sender, EventArgs e)
+        {
+            string host = "10.4.134.123:8124";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://" + host + "?cmd=control");
+            request.Method = "POST";
+            request.ContentType = "text/html";
+
+            string postJSON = "{\"mode\":\"control\",\"lights\":[{\"id\":\"6\",\"state\":\"off\"}]}";
+
+            byte[] buffer = Encoding.ASCII.GetBytes(postJSON);
+            Stream postData = request.GetRequestStream();
+            postData.Write(buffer, 0, buffer.Length);
+            postData.Close();
+
+            Stream response = request.GetResponse().GetResponseStream();
+            StreamReader responseReader = new StreamReader(response);
+            string responseJSON = responseReader.ReadToEnd();
+        }
+    }
+}
