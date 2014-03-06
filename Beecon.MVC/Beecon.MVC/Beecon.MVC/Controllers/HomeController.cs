187a099cebefa83dc6c9edc96d43c994d3c45b6d
joerythor <thor5728@fvtc.edu>
2014-03-05 19:19:46 -0600

    Started on Json Operators in MVC 4

---
+++ Beecon.MVC/Beecon.MVC/Beecon.MVC/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace Beecon.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+
+        #region JSON Operators
+
+        public ActionResult AllUsers()
+        {
+            return View();
+        }
+
+        public ActionResult AllBeecon()
+        {
+            return View();
+        }
+
+        public ActionResult BeeconUser()
+        {
+            return View();
+        }
+
+        public ActionResult Beecon()
+        {
+            return View();
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        public ActionResult CreateBeecon()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteUser()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteBeecon()
+        {
+            return View();
+        } 
+
+        #endregion
+
+    }
+}
---
+++ Beecon.MVC/Beecon.MVC/Beecon.MVC/Models/cUser.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecon.Models
+{
+	public class cUser
+	{
+		#region Properties
+
+		Guid id;
+		public Guid Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		string email;
+		public string Email 
+		{
+			get { return email;	}
+			set { email = value;}
+		}
+
+		string firstName;
+		public string FirstName 
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+
+		string lastName;
+		public string LastName 
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+
+		string gender;
+		public string Gender 
+		{
+			get { return gender; }
+			set { gender = value; }
+		}
+
+		string address;
+		public string Address 
+		{
+			get { return address; }
+			set { address = value; }
+		}
+
+		string city;
+		public string City 
+		{
+			get { return city; }
+			set { city = value;	}
+		}
+
+		string state;
+		public string State 
+		{
+			get { return state;	}
+			set { state = value; }
+		}
+
+		string zip;
+		public string Zip 
+		{
+			get { return zip; }
+			set { zip = value;}
+		}
+
+		DateTime dob;
+		public DateTime Dob 
+		{
+			get { return dob; }
+			set { dob = value; }
+		}
+
+		string password;
+		public string Password 
+		{
+			get { return password; }
+			set { password = value;	}
+		}
+
+		int beeconsFound;
+		public int BeeconsFound 
+		{
+			get { return beeconsFound; }
+			set { beeconsFound = value;	}
+		}
+
+		int beeconsCreated;
+		public int BeeconsCreated 
+		{
+			get { return beeconsCreated; }
+			set {beeconsCreated = value; }
+		}
+		#endregion
+	}
+}
+
