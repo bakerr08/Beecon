79d9866a9ca25f48900826eff871bc521fda515e
SeanCRobinson <robinson3665@fvtc.edu>
2014-04-01 10:19:14 -0500

    UI Progress

--- Beecon/Beecon/obj/Debug/android/beecon/beecon/R.java
+++ Beecon/Beecon/obj/Debug/android/beecon/beecon/R.java
@@ -14,16 +14,18 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int Email=0x7f050002;
+        public static final int Password=0x7f050004;
         public static final int SignIn=0x7f050005;
         public static final int SignUp=0x7f050006;
-        public static final int editText1=0x7f050002;
-        public static final int editText2=0x7f050004;
+        public static final int editText2=0x7f050007;
         public static final int linearLayout1=0x7f050000;
         public static final int textView1=0x7f050001;
         public static final int textView2=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int start=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
