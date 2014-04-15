a5ccc0965d36f3853e12125406e9dc3cef272922
aragorn55 <aragorn55@gmail.com>
2014-04-15 09:54:01 -0500

    BeeconDbExport
    export program start and sqlserver service start script start

---
+++ BeeconDbExport/BeeconDbExport.DL/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BeeconDbExport.DL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BeeconDbExport.DL")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8de67d62-e78d-466f-a371-4c93611d23d5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BeeconDbExport/BeeconDbExport.UI/cGetProcedures.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;      // For the database connections and objects.
+namespace BeeconDbExport.UI
+{
+    public class cGetProcedures
+    {
+        BeeconDB beeconDBD = new BeeconDB();
+
+        public void GetAllUsers()
+        {
+            //BeeconDBConnectionString
+            SqlConnection con = new SqlConnection(BeeconDbExport.UI.Properties.Settings.Default.BeeconDBConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "StoredProcedureName";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = con;
+
+            con.Open();
+
+            reader = cmd.ExecuteReader();
+            // Data is accessible through the DataReader object here.
+
+            con.Close();
+
+        }
+    }
+}
