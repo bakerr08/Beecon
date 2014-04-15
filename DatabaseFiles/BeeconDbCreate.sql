7b8567618d8c49a5c1d945fd3d992766c08603dd
aragorn55 <aragorn55@gmail.com>
2014-04-15 00:10:31 -0500

    all stored procedures added to the database creation script

--- DatabaseFiles/BeeconDbCreate.sql
+++ DatabaseFiles/BeeconDbCreate.sql
@@ -602,3 +602,107 @@ set UserID = @UserID,
  Created = @Created
 where InviteID = @InviteID;
 go
+use BeeconDB
+go
+create proc spDeleteInvitesByInviteID
+@InviteID INT
+as
+delete from Invites
+where  InviteID = @InviteID;
+GO
+
+create proc spDeleteTagByUserID
+ @UserID INT
+as
+delete from Tag
+where UserID = @UserID;
+
+go
+create proc spDeleteTagByID
+ @TagID INT
+as
+delete from TagRating
+where  TagID = @TagID;
+delete from TagVisited
+where  TagID = @TagID;
+delete  from Tag
+where  TagID = @TagID;
+delete from TagCatagory
+where  TagID = @TagID;
+go
+create proc spDeleteUsersByID
+@UserID INT
+as
+delete from TagVisited
+where  UserID = @UserID;
+delete from FriendList
+where  UserIDRequested = @UserID;
+delete from FriendList
+where  UserID = @UserID;
+delete from Invites
+where  UserIDSentTo = @UserID;
+delete from Invites
+where  UserID = @UserID;
+delete from Users
+where  UserID = @UserID;
+go
+create proc spDeleteTagRatingByID
+@RatingID INT
+as
+delete from TagRating
+where  RatingID = @RatingID;
+
+go
+create proc spDeleteCategoryByID
+@CategoryID INT
+as
+delete from TagCategory
+where  CategoryID = @CategoryID;
+delete from Category
+where  CategoryID = @CategoryID;
+
+go
+create proc spDeleteTagCategoryByTagID
+@TagID INT
+as
+delete from TagCategory
+where  TagID = @TagID;
+
+go
+create proc spDeleteFriendListByID
+@FriendID INT
+as
+delete from FriendList
+where  FriendID = @FriendID;
+
+go
+create proc spDeleteFriendListByUserID
+@UserID INT
+as
+delete from FriendList
+where  UserID = @UserID;
+
+go
+create proc spDeleteTagVisitedByUserID
+@UserID INT
+as
+delete from TagVisited
+where  UserID = @UserID;
+
+go
+create proc spDeleteTagVisitedByVisitID
+@VisitID INT
+as
+delete from TagVisited
+where  VisitID = @VisitID;
+go
+
+create proc spDeleteInvitesByUserID
+@UserID INT
+as
+delete from Invites
+where  UserID = @UserID;
+
+go
+
+
