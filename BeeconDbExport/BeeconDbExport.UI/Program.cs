ec87f1b5f82b2664fabaf8750a0222c88f8bedc9
aragorn55 <aragorn55@gmail.com>
2014-04-17 09:59:16 -0500

    dbexport

--- BeeconDbExport/BeeconDbExport.UI/Program.cs
+++ BeeconDbExport/BeeconDbExport.UI/Program.cs
@@ -8,8 +8,20 @@ namespace BeeconDbExport.UI
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            cGetProcedures oGet = new cGetProcedures();
+            List<string> oUsers = oGet.GetAllUsers();
+            Console.WindowWidth = 172;
+            foreach (string user in oUsers)
+            {
+                Console.WriteLine(user);
+
+
+            }
+            
+            Console.ReadLine();
         }
     }
 }
--- BeeconDbExport/BeeconDbExport.UI/cGetProcedures.cs
+++ BeeconDbExport/BeeconDbExport.UI/cGetProcedures.cs
@@ -11,15 +11,15 @@ namespace BeeconDbExport.UI
     public class cGetProcedures
     {
         BeeconDB beeconDBD = new BeeconDB();
-
-        public void GetAllUsers()
+        List<string> sUsers = new List<string>();
+        public List<string> GetAllUsers()
         {
             //BeeconDBConnectionString
             SqlConnection con = new SqlConnection(BeeconDbExport.UI.Properties.Settings.Default.BeeconDBConnectionString);
             SqlCommand cmd = new SqlCommand();
             SqlDataReader reader;
 
-            cmd.CommandText = "StoredProcedureName";
+            cmd.CommandText = "spGetAllUsers";
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Connection = con;
 
@@ -27,6 +27,27 @@ namespace BeeconDbExport.UI
 
             reader = cmd.ExecuteReader();
             // Data is accessible through the DataReader object here.
+            while (reader.Read())
+            {
+                string sUser = "";
+                for (int i = 0; i < 10; i++)
+			{
+               
+                    sUser = SafeGetString(reader, i) + ",";
+                    
+			}
+                sUsers.Add(sUser);
+              // if (!reader.IsDBNull(indexFirstName))
+              //  {
+           //         employee.FirstName = reader.GetString(indexFirstName);
+           //     }
+		  
+               // sUser += reader.GetInt32(0) + ", ";
+				
+				
+              
+				
+				}
 
             con.Close();
 			// oleDbCommand1.CommandText = "UpdateAuthor";
@@ -50,7 +71,34 @@ namespace BeeconDbExport.UI
 
 			// returnValue = (int)(oleDbCommand1.Parameters["retvalue"].Value);
 			// MessageBox.Show("Return Value = " + returnValue.ToString());
-
+            return sUsers;
+        }
+        public static string SafeGetString(SqlDataReader reader, int colIndex)
+        {
+            if (!reader.IsDBNull(colIndex))
+            {
+                var firstColType = reader.GetFieldType(colIndex);
+                string stype = firstColType.Name;
+                switch (stype)
+                {
+                    case "String":
+                        return reader.GetString(colIndex);
+                    case "Int32":
+                        return reader.GetInt32(colIndex).ToString();
+                    case "DateTime":
+                        return reader.GetDateTime(colIndex).ToString();
+                    case "Double":
+                        return reader.GetDouble(colIndex).ToString();
+                    case "Boolean":
+                        return reader.GetBoolean(colIndex).ToString();
+                    default:
+                        return "";
+                }
+                
+            }
+            else
+                return string.Empty;
         }
     }
+
 }
