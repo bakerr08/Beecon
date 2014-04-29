df4ee5ebcbc37a902c0f12d7f4e7541f4b1df01c
SeanCRobinson <robinson3665@fvtc.edu>
2014-04-29 01:09:08 -0500

    Added missing Activities

---
+++ Beecon/Beecon/SetProxActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Beecon
+{
+	[Activity (Label = "SetProxActivity")]			
+	public class SetProxActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.SetProx);
+			// Create your application here
+		}
+	}
+}
+
--- Beecon/Beecon/obj/Debug/android/beecon/beecon/R.java
+++ Beecon/Beecon/obj/Debug/android/beecon/beecon/R.java
@@ -39,10 +39,15 @@ public final class R {
         public static final int txtPasswordSignUp=0x7f050009;
     }
     public static final class layout {
-        public static final int create=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int signup=0x7f030002;
-        public static final int start=0x7f030003;
+        public static final int beecontext=0x7f030000;
+        public static final int create=0x7f030001;
+        public static final int friends=0x7f030002;
+        public static final int home=0x7f030003;
+        public static final int leaderboard=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int setprox=0x7f030006;
+        public static final int signup=0x7f030007;
+        public static final int start=0x7f030008;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
