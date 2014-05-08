ee856abda533bfade8e28176b0ec8209b558f131
Sean Robinson <seancr93@gmail.com>
2014-05-08 10:02:43 -0500

    Geolocation Code

--- Beecon/Beecon/BeeconTextActivity.cs
+++ Beecon/Beecon/BeeconTextActivity.cs
@@ -8,6 +8,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Xamarin.Geolocation;
 
 namespace Beecon
 {
@@ -21,10 +22,33 @@ namespace Beecon
 			Button Submit = FindViewById<Button> (Resource.Id.btnSubmitBeecon);
 			String BeeconTxt = (Resource.Id.txtBeeconText);
 
+			string Time;
+			string Latitude;
+			string Longitude;
+			string Proximity;
+			Geolocator geolocator;
+
+			var locator = new Geolocator (this) {DesiredAccuracy=50};
+			this.geolocator.GetPositionAsync (timeout:10000) .CountinueWith (t => {
+
+				Time.text = t.Result.Timestamp.ToString("G");
+				Proximity.text = t.Result.Accuracy;
+				Latitude.text = t.Result.Latitude.ToString("N4");
+				Longitude.text = t.Result.Longitude.ToString("N4");
+
+			}, TaskScheduler.FromCurrentSynchronizationContext());
+
+
+
+			btnSubmitBeecon.Click += (sender, e) =>  {
 			if (BeeconTxt == String.Empty)
 				return;
 
-			// Take Beecon Text Send Serialize it to JSON and Send it
+				// Take Beecon Text Send Serialize it to JSON and Send it
+
+
+			}
+
 
 		}
 	}
