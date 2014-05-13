c426751c20a60e1b6fb0063d8bd69075a33aa991
aragorn55 <aragorn55@gmail.com>
2014-05-12 22:01:46 -0500

    beecon json

--- Beecon/Beecon.Models2/cBeecon.cs
+++ Beecon/Beecon.Models2/cBeecon.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Collections;
+using System.Dynamic;
+using System.ComponentModel;
 
 namespace Beecon.Models2
 {
@@ -116,6 +122,96 @@ namespace Beecon.Models2
 
 
 		}
+        public string PostDataWithOperation(string operation, string JSON)
+        {
+            var httpReq = (HttpWebRequest)HttpWebRequest.Create(string.Format("{0}/{1}", getTargetUrl(), operation));
+
+            httpReq.BeginGetResponse((ar) =>
+            {
+
+                var request = (HttpWebRequest)ar.AsyncState;
+                //set http method
+                request.Method = "POST";
+                //content type
+                request.ContentType = "text/html";
+                byte[] buffer = Encoding.UTF8.GetBytes(JSON);
+                using (var response = (HttpWebResponse)request.EndGetResponse(ar))
+                {
+
+                    //var s = response.GetResponseStream ();
+                    Stream PostData = response.GetResponseStream();
+                    StreamReader response_reader = new StreamReader(PostData);
+                    string response_json = response_reader.ReadToEnd();
+                    response_json = processResponse(response_json);
+                    //RunOnUiThread (() => {
+                    LoadJson(response_json);
+                    
+                }
+
+            }, httpReq);
+
+            return "Success";
+        }
+        public string processResponse(string response_json)
+        {
+            //puts the response into a dynamic object
+            //dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(response_json);
+            Dictionary<string, string> data = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(response_json);
+
+            if (data["operation"] == "SignIn")//Name of the opperation 
+            {
+                //check for success
+                if (data["message"] == "Success")
+                {
+                    FromData(data);//deserilize the JSON into a beecon
+                    return data["message"]; //return the Success to the form
+                }
+                else
+                {
+                    return data["message"]; //return the Fail to the form
+                }
+            }
+            else if (data["operation"] == "allgames")
+            {
+                return data["message"];
+            }
+
+            return "true";
+        }
+        public static cBeecon FromData(Dictionary<string, string> data)
+        {
+            cBeecon beecon = new cBeecon();
+            beecon.beeconID = Convert.ToInt32(data["tagid"]);
+            beecon.beeconLongitude = data["taglongitude”"];
+            beecon.beeconLatitude = data["“taglatitude”"];
+            beecon.beeconDescription = data["tagdescription"];
+            beecon.beeconDateCreated = Convert.ToDateTime(data["tagdatecreated"]);
+            beecon.beeconDateExpired = Convert.ToDateTime(data["tagexpired"]);
+            beecon.userId = Convert.ToInt32(data["userid"]);
+            beecon.beeconContentURL = data["tagcontent_url"];
+            beecon.beeconPrivacyTypeID = Convert.ToInt32(data["privacytypeid"]);
+            return beecon;
+        }
+        public string getTargetUrl()
+        {
+            string targetURL = "http://itweb.fvtc.edu/beecon/Beecon";
+            return targetURL;
+        }
+        public void LoadJson(string json)
+        {
+            var beecon = JsonConvert.DeserializeObject<cBeecon>(json);
+            beeconID = beecon.beeconID;
+            beeconLongitude = beecon.beeconLongitude;
+            beeconLatitude = beecon.beeconLatitude;
+            beeconDescription = beecon.beeconDescription;
+            beeconDateCreated = beecon.beeconDateCreated;
+            beeconDateExpired = beecon.beeconDateExpired;
+            userId = beecon.userId;
+            beeconContentURL = beecon.beeconContentURL;
+            beeconPrivacyTypeID = beecon.beeconPrivacyTypeID;
+            
+
+        }
 	}
 }
 
